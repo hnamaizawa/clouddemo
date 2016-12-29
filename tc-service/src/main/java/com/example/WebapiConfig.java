65e58e4258618f87e6a0c93a64fd0302827477e3
tkotegaw <tadahisa.kotegawa@oracle.com>
2016-12-29 22:56:23 +0900

    change to the JAX-RS compliant way

--- tc-service/src/main/java/com/example/WebapiConfig.java
+++ tc-service/src/main/java/com/example/WebapiConfig.java
@@ -1,14 +1,21 @@
 package com.example;
 
-import javax.ws.rs.ApplicationPath;
+import java.util.HashSet;
+import java.util.Set;
 
-import org.glassfish.jersey.server.ResourceConfig;
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+//import org.glassfish.jersey.server.ResourceConfig;
 
 import com.example.auth.AuthFilter;
+import com.example.auth.AuthRestService;
+import com.example.auth.IDCSOAuthService;
 import com.example.filters.CORSFilter;
 
-@ApplicationPath("")
-public class WebapiConfig extends ResourceConfig {
+@ApplicationPath("/")
+public class WebapiConfig extends /*ResourceConfig*/ Application{
+/*
 	public WebapiConfig() {
 //		packages(this.getClass().getPackage().getName());
 //		packages("com.github.kamegu.first");
@@ -18,4 +25,21 @@ public class WebapiConfig extends ResourceConfig {
         register(AuthFilter.class);	
         register(GlobalExceptionMapper.class);	
     }
+*/
+
+	@Override
+	public Set<Class<?>> getClasses() {
+        Set<Class<?>> s = new HashSet<Class<?>>();
+        s.add(MyResource.class);
+        s.add(TestRestService.class);
+        s.add(DatabaseRestService.class);
+        s.add(DatabaseRestServiceSample.class);
+        s.add(AuthRestService.class);
+        s.add(IDCSOAuthService.class);
+        s.add(CORSFilter.class);
+        s.add(AuthFilter.class);
+        s.add(GlobalExceptionMapper.class);
+        return s;
+    }
+
 }
